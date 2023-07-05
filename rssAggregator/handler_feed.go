8f02973c3db672e259ab42c6dc5d0b530ba1afe9
robinmuhia <muhiarobinonyancha@gmail.com>
2023-07-05 21:19:50 +0300

    Get feeds functionality added

--- rssAggregator/handler_feed.go
+++ rssAggregator/handler_feed.go
@@ -38,6 +38,11 @@ func (apiConfig *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Req
 	respondWithJson(w,201,databaseFeedToFeed(feed))
 }
 
-// func (apiConfig *apiConfig) handlerGetFeed(w http.ResponseWriter, r *http.Request, user database.User){
-// 	respondWithJson(w,200,databaseUserToUser(user))
-// }
+func (apiConfig *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request){
+	feeds,err := apiConfig.DB.GetFeeds(r.Context())
+	if err != nil{
+		respondWithError(w,400,fmt.Sprintf("Couldn't get feeds:%v",err))
+		return
+	}
+	respondWithJson(w,200,databaseFeedsToFeeds(feeds))
+}
